752f475e580429cf657129675e77fe0294cd8756
miloa2002 <agudelo.tapias@gmail.com>
2024-11-05 20:43:39 -0500

    Feat: Cors frontend

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import colors from "colors";
 import conectDB from "./config/db.js";
 import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
 
 const app = express();
 
@@ -10,10 +11,16 @@ dotenv.config();
 
 conectDB();
 
+const corsOption = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOption));
 app.use(express.json());
 app.use("/api/v1/user", userRoutes);
 
 const PORT = 4000 | process.env.PORT;
 app.listen(PORT, () => {
-    console.log(colors.green(`Port listening on the server ${PORT}`));
-})
+  console.log(colors.green(`Port listening on the server ${PORT}`));
+});
